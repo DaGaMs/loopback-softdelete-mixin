80e5e226e23f1913798794802a1371cdf53d1ad0
Benjamin Schuster-Boeckler <ben@evershelfapp.com>
2017-06-25 14:07:44 +0100

    fixed brain fart

--- src/soft-delete.js
+++ src/soft-delete.js
@@ -64,11 +64,12 @@ export default (Model, { deletedAt = 'deletedAt', _isDeleted = '_isDeleted', scr
 
   const _findOrCreate = Model.findOrCreate;
   Model.findOrCreate = function findOrCreateDeleted(query = {}, ...rest) {
-    if (!query.deleted && !query[_isDeleted]) {
+    if (!query.deleted) {
       if (!query.where || _.isEmpty(query.where)) {
         query.where = queryNonDeleted;
       } else {
-        query.where = { and: [ query.where, queryNonDeleted ] };
+        if (!query.where[_isDeleted]) // this should be a deep check!
+          query.where = { and: [ query.where, queryNonDeleted ] };
       }
     }
 
@@ -77,11 +78,12 @@ export default (Model, { deletedAt = 'deletedAt', _isDeleted = '_isDeleted', scr
 
   const _find = Model.find;
   Model.find = function findDeleted(query = {}, ...rest) {
-    if (!query.deleted && !query[_isDeleted]) {
+    if (!query.deleted) {
       if (!query.where || _.isEmpty(query.where)) {
         query.where = queryNonDeleted;
       } else {
-        query.where = { and: [ query.where, queryNonDeleted ] };
+        if (!query.where[_isDeleted]) // this should be a deep check!
+          query.where = { and: [ query.where, queryNonDeleted ] };
       }
     }
 
